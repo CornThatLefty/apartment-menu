b97b7fa6ff07d98e813fb5ad2891db6d57a1e3b8
Stephen Chavez <stephen@dicesoft.net>
2017-01-31 13:31:30 -0700

    Refactored JS
    This will allow us to use a MongoDB now.

--- aptmenu_client.js
+++ aptmenu_client.js
@@ -1,168 +1,200 @@
 ﻿
 //todo : clean shit up, probably use enums or something
 
-menuPool = API.getMenuPool();
+// Debug option
+let debugging = true;
 
 //Begin main menu -----------------------------------------------------------------------
 
-let aptNames = ["Modern Apartment", "Moody Apartment", "Vibrant Apartment",
-                "Sharp Apartment", "Monochrome Apartment", "Seductive Apartment",
-                "Regal Apartment", "Aqua Apartment"];
+let menuPool = null;
 
-function setupMenu(aptNames) {
-  let menu = API.createMenu("Apartments", 0, 0, 6);
-  let floor1Menu = API.createMenu("Floor 1", 0, 0, 6);
-  let floor2Menu = API.createMenu("Floor 2", 0, 0, 6);
-  let floor3Menu = API.createMenu("Floor 3", 0, 0, 6);
+let floorMenus = [];
+let aptMenu = null;
+//Prefix explanation -- 1st #:Floor number; 2nd and 3rd #: Apt number
+let aptNames =  [
+        {
+            "name": "Modern Apartment",
+            "floorNum": 3, 
+            "floorData": [
+                "loadApt101",
+                "loadApt201",
+                "loadApt301"
+            ],
+        },
+        {
+            "name": "Moody Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt102",
+                "loadApt202",
+                "loadApt302",
+            ],
+        },
+        {
+            "name": "Vibrant Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt103",
+                "loadApt203",
+                "loadApt303",
+            ],
+        },
+        {
+            "name": "Sharp Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt104",
+                "loadApt204",
+                "loadApt304",
+            ],
+        },
+        {
+            "name": "Monochrome Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt105",
+                "loadApt205",
+                "loadApt305",
+            ],
+        },
+        {
+            "name": "Seductive Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt106",
+                "loadApt206",
+                "loadApt306",
+            ],
+        },
+        {
+            "name": "Regal Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt107",
+                "loadApt207",
+                "loadApt307",
+            ],
+        },
+        {
+            "name": "Aqua Apartment",
+            "floorNum": 3,
+            "floorData": [
+                "loadApt108",
+                "loadApt208",
+                "loadApt308",
+            ],
+        },
+        {
+            "name": "Executive Rich",
+            "floorNum": 9,
+            "floorData": [
+                "loadApt401",
+                "loadApt402",
+                "loadApt403",
+                "loadApt404",
+                "loadApt405",
+                "loadApt406",
+                "loadApt407",
+                "loadApt408",
+                "loadApt409",
+            ],
+        }
+    ]
 
-  let sub1 = API.createMenuItem("Floor 1", "");
-  let sub2 = API.createMenuItem("Floor 2", "");
-  let sub3 = API.createMenuItem("Floor 3", "");
+setupMenus(aptNames);
+  
+function setupMenus(aptNames) {
+    menuPool = API.getMenuPool();
+    
+    floorMenus = createFloorMenus(aptNames);
 
+    floorMenus.forEach(function (subMenu) {
+        menuPool.Add(subMenu);
+    });
 
-  menu.AddItem(sub1);
-  menu.AddItem(sub2);
-  menu.AddItem(sub3);
-
-  // Loop through all the names.
-  aptNames.forEach(function (name) {
-      tempItem1 = API.createMenuItem(item, "Floor 1"));
-      tempItem2 = API.createMenuItem(item, "Floor 2"));
-      tempItem3 = API.createMenuItem(item, "Floor 3"));
-      floor1Menu.AddItem(tempItem1);
-      floor2Menu.AddItem(tempItem2);
-      floor3Menu.AddItem(tempItem3);
-    }
-
-    menuPool.Add(menu);
-    menuPool.Add(menu1);
-    menuPool.Add(menu2);
-    menuPool.Add(menu3);
-
+    aptMenu = createAptMenu(aptNames, floorMenus);
+    menuPool.Add(aptMenu);
 }
 
+function createFloorMenus(aptNames) {
 
-setupMenu(aptNames);
-let keyDetect = false;
+    let floorMenus = [];
+
+    // Loop through all the apartments
+    aptNames.forEach(function (apt) {
+        // Create the floor menus of apartments.
+
+        API.sendChatMessage(apt.name);
+        tempFloorMenu = API.createMenu(apt.name, "Floor Selection", 0, 0, 6);
+
+        for (let i = 0; i < apt.floorData.length; i++) {
+            floorMenuItem = API.createMenuItem("Floor " + (i + 1), "");
+
+            // Assign load function to the item.
+            floorMenuItem.Activated.connect(function (menu, item) {
+                API.triggerServerEvent(apt.floorData[i]);
+
+                menu.Visible = false;
+            });
+
+            tempFloorMenu.AddItem(floorMenuItem);
+        }
+
+        floorMenus.push(tempFloorMenu);
+    });
+
+    return floorMenus;
+}
+
+function createAptMenu(aptNames, floorMenus) {
+    let menu = API.createMenu("Apartments", 0, 0, 6);
+
+    let tempMenuIndex = 0;
+
+    // Loop through all the apartments
+    for(let i = 0; i < aptNames.length; i++) {
+        // Create the main menu of apartments.
+
+        mainMenuNameItem = API.createMenuItem(aptNames[i].name, "");
+
+        // Assign floor menu to each apartment menu item.
+        mainMenuNameItem.Activated.connect(function (menu, item) {
+            // opens floor menu 
+            API.sendChatMessage(i.toString());
+
+            menu.Visible = false;
+
+            floorMenus[i].Visible = true;
+        });
+
+        tempMenuIndex++;
+        menu.AddItem(mainMenuNameItem);
+
+    }
+
+    return menu;
+}
 
 //End main menu -----------------------------------------------------------------------
 
-menu1.OnItemSelect.connect(function(sender, item, index) { //Prefix explanation -- 1st #:Floor number; 2nd and 3rd #: Apt number
-    switch (item) {
-        case item1:
-            API.triggerServerEvent("loadApt101");
-            break;
-        case item4:
-            API.triggerServerEvent("loadApt102");
-            break;
-        case item7:
-            API.triggerServerEvent("loadApt103");
-            break;
-        case item10:
-            API.triggerServerEvent("loadApt104");
-            break;
-        case item13:
-            API.triggerServerEvent("loadApt105");
-            break;
-        case item16:
-            API.triggerServerEvent("loadApt106");
-            break;
-        case item19:
-            API.triggerServerEvent("loadApt107");
-            break;
-        case item22:
-            API.triggerServerEvent("loadApt108");
-            break;
-    }
-    menu1.Visible = false;
-});
-
-menu2.OnItemSelect.connect(function (sender, item, index) { //Prefix explanation -- 1st #:Floor number; 2nd and 3rd #: Apt number
-    switch (item) {
-        case item2:
-            API.triggerServerEvent("loadApt201");
-            break;
-        case item5:
-            API.triggerServerEvent("loadApt202");
-            break;
-        case item8:
-            API.triggerServerEvent("loadApt203");
-            break;
-        case item11:
-            API.triggerServerEvent("loadApt204");
-            break;
-        case item14:
-            API.triggerServerEvent("loadApt205");
-            break;
-        case item17:
-            API.triggerServerEvent("loadApt206");
-            break;
-        case item20:
-            API.triggerServerEvent("loadApt207");
-            break;
-        case item23:
-            API.triggerServerEvent("loadApt208");
-            break;
-    }
-    menu2.Visible = false;
-});
-
-menu3.OnItemSelect.connect(function (sender, item, index) { //Prefix explanation -- 1st #:Floor number; 2nd and 3rd #: Apt number
-    switch (item) {
-        case item3:
-            API.triggerServerEvent("loadApt301");
-            break;
-        case item6:
-            API.triggerServerEvent("loadApt302");
-            break;
-        case item9:
-            API.triggerServerEvent("loadApt303");
-            break;
-        case item12:
-            API.triggerServerEvent("loadApt304");
-            break;
-        case item15:
-            API.triggerServerEvent("loadApt305");
-            break;
-        case item18:
-            API.triggerServerEvent("loadApt306");
-            break;
-        case item21:
-            API.triggerServerEvent("loadApt307");
-            break;
-        case item24:
-            API.triggerServerEvent("loadApt308");
-            break;
-    }
-    menu3.Visible = false;
-});
-
-menu.OnItemSelect.connect(function (sender, item, index) {
-    switch (item) {
-        case sub1:
-            menu.Visible = false;
-            menu1.Visible = true;
-            break;
-        case sub2:
-            menu.Visible = false;
-            menu2.Visible = true;
-            break;
-        case sub3:
-            menu.Visible = false;
-            menu3.Visible = true;
-            break;
-    }
-    API.showCursor(false);
-    menu.Visible = false;
-});
 
 API.onKeyDown.connect(function (sender, e) {
-    if (keyDetect == true) {
-        if (e.KeyCode == Keys.E) {
-            menu.Visible = true;
-            menu1.Visible = false;
-            menu2.Visible = false;
-            menu3.Visible = false;
+    if (debugging) {
+        if (!aptMenu.Visible && e.KeyCode == Keys.E && !API.isChatOpen()) {
+            aptMenu.Visible = true;
+
+            floorMenus.forEach(function (subMenu) {
+                subMenu.Visible = false;
+            });
+
+            API.showCursor(false);
+        } else if (e.KeyCode == Keys.E && !API.isChatOpen()){
+            aptMenu.Visible = false;
+
+            floorMenus.forEach(function (subMenu) {
+                subMenu.Visible = false;
+            });
+
             API.showCursor(false);
         }
     }
@@ -180,9 +212,10 @@ API.onServerEventTrigger.connect(function (eventName, args) { //detects triggers
             keyDetect = false;
             API.setHudVisible(true);
             menu.Visible = false;
-            menu1.Visible = false;
-            menu2.Visible = false;
-            menu3.Visible = false;
+            floor1Menu.Visible = false;
+            floor2Menu.Visible = false;
+            floor3Menu.Visible = false;
+            executiveMenu.Visible = false;
 
             break;
     }
@@ -191,3 +224,4 @@ API.onServerEventTrigger.connect(function (eventName, args) { //detects triggers
 API.onUpdate.connect(function (sender, events) {
     menuPool.ProcessMenus();
 });
+        
